1061edc3a2242857d5105e5e11ae3b788ae4747c
Brett Levine <brett@concreteinteractive.com>
2012-08-27 16:03:26 -0700

    - Added some hints for Facebook app creation

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -233,6 +233,8 @@ Devise.setup do |config|
   raise "Go register your app with dev.Twitter.com and put the consumer key and secret into your environment." if ENV["TWITTER_CONSUMER_KEY"].nil? or ENV["TWITTER_CONSUMER_SECRET"].nil?
   config.omniauth :twitter, ENV["TWITTER_CONSUMER_KEY"], ENV["TWITTER_CONSUMER_SECRET"]
 
+  #Hint: When you register your app with Facebook, enable sandbox mode and add your development team to it (so it doesn't spam your friends)
+  #Hint: set your redirect URL to http://localhost:3000 if you want to use twitter in your development environment (create a separate Facebook app for production)
   raise "Go register your app with developer.Facebook.com and put the app ID and app secret into your environment." if ENV["FACEBOOK_APP_ID"].nil? or ENV["FACEBOOK_APP_SECRET"].nil?
   config.omniauth :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_APP_SECRET"]
 end
