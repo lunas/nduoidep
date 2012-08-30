b66fa5e6c166fea5465597b5ee2eb46e06ea0f10
Tumas Bajoras <tumas.bajoras@necolt.com>
2012-08-30 18:12:06 +0300

    adding user factory & adding it to seed

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,12 @@
 require 'factory_girl'
 
 FactoryGirl.define do
+  factory :user do
+    username { Faker::Name.first_name.downcase }
+    email { Faker::Internet.email }
 
-
+    password "foobar"
+    password_confirmation "foobar"
+  end
 end
 
