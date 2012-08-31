1caa7ddf581ff60d26447b61c4c4218227e29e54
Brett Levine <brett@concreteinteractive.com>
2012-08-31 12:05:11 -0700

    - Use Factories in our seeds file - Move User factory to spec/factories - Create sequential seed files in db/seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,6 @@
-FactoryGirl.create :user
+require 'factory_girl'
+# See: http://xtargets.com/2011/02/06/seeding-your-rails-db-with-factory-girl/
+Dir[Rails.root.join('db', 'seeds', '*.rb').to_s].sort.each do |file|
+  puts "Loading db/seeds/#{file.split(File::SEPARATOR).last}"
+  load(file)
+end
