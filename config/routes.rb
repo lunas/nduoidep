cea51bd43e62f192e03aff341108556c22202a25
Lukas <lukas@builditconcrete.com>
2013-02-24 22:06:46 -0800

    removed seed contrl and views, newrelic.yml, routes, and login-info links for main page

--- config/routes.rb
+++ config/routes.rb
@@ -9,16 +9,19 @@ Blog::Application.routes.draw do
   get "registrations/edit"
 
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"},
-                     controllers: {:registrations => "registrations"}
+             controllers: {
+                 :registrations => "registrations",
+                 :confirmations => "confirmations",
+                 :passwords => "passwords"
+             }
+
+  #devise_for :users, path_names: {sign_in: "login", sign_out: "logout"},
+  #                   controllers: {:registrations => "registrations"}
+
   Devise.router_name = :main_app
 
-  root to: 'seed#index'
 
-  resources :seed, :only => [:index]
-
-  match :show, to: 'seed#show'  # just an example route to demonstrate cancan's authorization!
-  match :edit, to: 'seed#edit'  # just an example route to demonstrate cancan's authorization!
-
+  root to: 'issues#index'
 
   #resque_constraint = lambda do |request|
   #  request.env['warden'].authenticate? and request.env['warden'].user.role? :admin
