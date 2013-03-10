ed72763e0c8833c87e3a67cade7509933547dc93
Lukas <lukas@builditconcrete.com>
2013-03-10 15:21:44 -0700

    controls to select issue/page/company on issue show page

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -6,9 +6,29 @@ class Issue < ActiveRecord::Base
 
   scope :latest, order("date DESC").limit(1)
 
+  def self.all_issues_with_id
+    order("date DESC").map { |issue| [issue.title + ' -- ' + issue.date.to_s, issue.id, ] }
+  end
+
   def first_pages
     pages.order("page_nr ASC").limit(5)
   end
+
+  # Return array of
+  def companies
+    #sql = "select distinct c.name
+    #       from (issues as i inner join pages as p on i.id=p.issue_id)
+    #       inner join ads as a on p.id=a.page_id
+    #       inner join companies as c on a.company_id=c.id
+    #       where i.id = #{id};"
+    Issue.select("companies.id, companies.name")
+         .joins(pages: {ads: :company})
+         .where("issues.id=#{id}")
+         .order("companies.name ASC")
+         .uniq
+         .map{|c| [c.name, c.id] }
+  end
+
 
   # Typus helper methods:
 
